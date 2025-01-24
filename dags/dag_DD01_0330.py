8b095ba931e5be6b70f17238e2c74138a6f59132
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2025-01-24 17:21:25 +0900

    수정

--- dags/dag_DD01_0330.py
+++ dags/dag_DD01_0330.py
@@ -1,10 +1,5 @@
 from airflow import DAG
-from airflow.decorators import task
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-import pandas as pd
-import boto3
-import json
 
 
 with DAG(
@@ -12,9 +7,46 @@ with DAG(
     schedule_interval=None,
     tags=["현대홈쇼핑","100_COM"]
 ) as dag:
-    @task(task_id="python_task_1")
-    def print_context(some_input):
-        print(some_input)
+    trigger_dag_CDC_ODS_DAILY_HMALL_TO_HDHS_01 = TriggerDagRunOperator(
+        task_id='trigger_dag_CDC_ODS_DAILY_HMALL_TO_HDHS_01',
+        trigger_dag_id='dag_CDC_ODS_DAILY_HMALL_TO_HDHS_01',
+        reset_dag_run=True,         # 이미 수행된 dag여도 수행 할 것인지
+        wait_for_completion=True,  # 트리거 하는 dag가 끝날때까지 기다릴 것인지
+        poke_interval=60,
+        allowed_states=['success'], # 트리거 하는 dag가 어떤 상태여야 오퍼레이터가 성공으로 끝나는지
+        trigger_rule="all_done"
+    )
 
+    trigger_dag_DD01_0330_CMS_01 = TriggerDagRunOperator(
+        task_id='trigger_dag_DD01_0330_CMS_01',
+        trigger_dag_id='dag_DD01_0330_CMS_01',
+        reset_dag_run=True,         # 이미 수행된 dag여도 수행 할 것인지
+        wait_for_completion=True,  # 트리거 하는 dag가 끝날때까지 기다릴 것인지
+        poke_interval=60,
+        allowed_states=['success'], # 트리거 하는 dag가 어떤 상태여야 오퍼레이터가 성공으로 끝나는지
+        trigger_rule="all_done"
+    )
 
-    python_task_1 = print_context('task_decorator 실행')
+    trigger_dag_DD01_0330_DAU_SMS_01 = TriggerDagRunOperator(
+        task_id='trigger_dag_DD01_0330_DAU_SMS_01',
+        trigger_dag_id='dag_DD01_0330_DAU_SMS_01',
+        reset_dag_run=True,         # 이미 수행된 dag여도 수행 할 것인지
+        wait_for_completion=True,  # 트리거 하는 dag가 끝날때까지 기다릴 것인지
+        poke_interval=60,
+        allowed_states=['success'], # 트리거 하는 dag가 어떤 상태여야 오퍼레이터가 성공으로 끝나는지
+        trigger_rule="all_done"
+    )
+
+    trigger_dag_DD01_0330_ON_DEMAND_01 = TriggerDagRunOperator(
+        task_id='trigger_dag_DD01_0330_ON_DEMAND_01',
+        trigger_dag_id='dag_DD01_0330_ON_DEMAND_01',
+        reset_dag_run=True,         # 이미 수행된 dag여도 수행 할 것인지
+        wait_for_completion=True,  # 트리거 하는 dag가 끝날때까지 기다릴 것인지
+        poke_interval=60,
+        allowed_states=['success'], # 트리거 하는 dag가 어떤 상태여야 오퍼레이터가 성공으로 끝나는지
+        trigger_rule="all_done"
+    )
+
+    [trigger_dag_CDC_ODS_DAILY_HMALL_TO_HDHS_01, trigger_dag_DD01_0330_CMS_01]
+
+    trigger_dag_DD01_0330_CMS_01 >> trigger_dag_DD01_0330_DAU_SMS_01 >> trigger_dag_DD01_0330_ON_DEMAND_01
--- dags/dag_DD01_0330_DAU_SMS_01.py
+++ dags/dag_DD01_0330_DAU_SMS_01.py
@@ -1,10 +1,7 @@
 from airflow import DAG
-from airflow.decorators import task
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from operators.etl_schedule_update_operator import etlScheduleUpdateOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-import pandas as pd
-import boto3
-import json
+
 
 
 with DAG(
@@ -12,9 +9,28 @@ with DAG(
     schedule_interval=None,
     tags=["현대홈쇼핑","dag_DD01_0330"]
 ) as dag:
-    @task(task_id="python_task_1")
-    def print_context(some_input):
-        print(some_input)
+    task_ETL_SCHEDULE_c_01 = etlScheduleUpdateOperator(
+        task_id="task_ETL_SCHEDULE_c_01"
+    )
 
+    trigger_dag_CDC_ODS_DAILY_CAMP_01 = TriggerDagRunOperator(
+        task_id='trigger_dag_CDC_ODS_DAILY_CAMP_01',
+        trigger_dag_id='dag_CDC_ODS_DAILY_CAMP_01',
+        reset_dag_run=True,  # 이미 수행된 dag여도 수행 할 것인지
+        wait_for_completion=True,  # 트리거 하는 dag가 끝날때까지 기다릴 것인지
+        poke_interval=60,
+        allowed_states=['success'],  # 트리거 하는 dag가 어떤 상태여야 오퍼레이터가 성공으로 끝나는지
+        trigger_rule="all_done"
+    )
 
-    python_task_1 = print_context('task_decorator 실행')
+    trigger_dag_CDC_ODS_DAU_SMS_01 = TriggerDagRunOperator(
+        task_id='trigger_dag_CDC_ODS_DAU_SMS_01',
+        trigger_dag_id='dag_CDC_ODS_DAU_SMS_01',
+        reset_dag_run=True,  # 이미 수행된 dag여도 수행 할 것인지
+        wait_for_completion=True,  # 트리거 하는 dag가 끝날때까지 기다릴 것인지
+        poke_interval=60,
+        allowed_states=['success'],  # 트리거 하는 dag가 어떤 상태여야 오퍼레이터가 성공으로 끝나는지
+        trigger_rule="all_done"
+    )
+
+    task_ETL_SCHEDULE_c_01 >> trigger_dag_CDC_ODS_DAILY_CAMP_01 >> trigger_dag_CDC_ODS_DAU_SMS_01
